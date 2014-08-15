4461ea013d6ee3004b79e5cef2196abffbdc05c3
Wojciech Cibor <wojciech.cibor@gmail.com>
2014-08-15 17:10:34 +0200

    Initial VS project.

---
+++ PgpFileEncryption/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PgpFileEncryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

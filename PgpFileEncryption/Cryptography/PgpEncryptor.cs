e8008024f88b84d793107d7f522d37f687ffa9b7
Wojciech Cibor <wojciech.cibor@gmail.com>
2014-08-15 20:04:24 +0200

    PgpEncryptor interface and implementation stub.

---
+++ PgpFileEncryption/Cryptography/PgpEncryptor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PgpFileEncryption.Cryptography
+{
+    public interface IPgpEncryptor
+    {
+        Stream Encrypt(Stream inputFile, Stream publicKey, bool asciiArmor = true);
+
+        Stream Decrypt(Stream inputFile, Stream pgpFile);
+    }
+
+    public class PgpEncryptor : IPgpEncryptor
+    {
+        public Stream Encrypt(Stream inputFile, Stream publicKey, bool asciiArmor = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream Decrypt(Stream inputFile, Stream pgpFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
